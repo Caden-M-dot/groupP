3b86ca81c0720ecae3cf4439c6325d67b64d27f5
Caden-M-dot <mcra0146@algonquinlive.com>
2026-04-13 20:32:33 -0400

    Done
    It's Done

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # groupP
 Group Project
+This is a simple program reworking a previous program into a storefront site.
+It connects to mongoDB to store database items.
+This is accessed through forms described in the html file.
+A test exists to ensure the functionality of the getAll endpoint.
--- automate.js
+++ automate.js
@@ -1,3 +1,19 @@
+const dbURI = process.env.DB_URI || 'mongodb://localhost';
+const express = require('express');
+const cors = require('cors');
+const db = require('./models/db')
+const Item = require('./models/items');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 const test= require('./tests/server.test.js')
 // Start the process
 test;
